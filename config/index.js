fadf86446f106c4cce1012c55ebc699f92e54228
shenjinhao <114160729+shenjinhao@users.noreply.github.com>
2022-09-22 23:06:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "e444fc761c11638434e9dfaae22e9291oPE5s5neU08UbmncrySgYtqeEtfw",
+  APP_SECRET: "5ded7468c4da0d603b4cf5b8f2a428b7",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
