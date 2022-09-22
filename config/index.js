f74f799ad72191698eab112ad60512d17e2bc255
shenjinhao <114160729+shenjinhao@users.noreply.github.com>
2022-09-22 23:03:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,13 +6,13 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx015d15d138fb0665",
 
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "e444fc761c11638434e9dfaae22e9291oPE5s5neU08UbmncrySgYtqeEtfw",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -147,7 +147,7 @@ export const USER_CONFIG = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oPE5s5neU08UbmncrySgYtqeEtfw",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -162,7 +162,7 @@ export const USER_CONFIG = {
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
   PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "肇庆",
   
   /** 重要节日相关 */
   
